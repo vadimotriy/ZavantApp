867b3cc731643ff0c8d00838fab45c16aa5a859f
vadimotriy <vblinov2009.rus@yandex.ru>
2026-02-19 21:00:49 +0300

    [FRONTEND] Идея проекта изменена. Изменен интерфейс

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("org.java-websocket:Java-WebSocket:1.5.3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/ikar_2026/ui/MainActivity.java
+++ app/src/main/java/com/example/ikar_2026/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ikar_2026.ui;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.ikar_2026.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "IKAR2026MainActivity";
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.button);
+
+        Log.i(LOG_TAG, "Активность создана");
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Log.i(LOG_TAG, "Пользователь нажал на кнопку");
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
